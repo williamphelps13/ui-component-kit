65bcb8820c39c01c49c2964dfc3c80f39dbe84eb
William J Phelps <williamjphelps@Williams-MacBook-Pro-2.local>
2023-03-11 19:43:55 -0700

    Add Chromatic deploy Storybook Github action

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
+<script>
+	import Button from '../lib/stories/Button.svelte';
+</script>
+
 <h1>Welcome to your library project</h1>
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Button primary label="Test" />
